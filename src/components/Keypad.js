654a20d68db563a0138ffbd3abb1d5295df9eb25
matthewmwachi <mattmuhande@gmail.com>
2024-04-29 11:11:11 +0300

    event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
+    function pass(){
+        console.log(`Entering password...`)
+    }
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type ="password" name ="password" onChange = {pass}/>
+            </form>
+        </div>
     )
 }
 
